6a6982cbaaca8712c60feb3d2d24b3eb9e827f09
alejandrodh <alejandro@digitalhouse.com>
2020-05-08 07:08:47 -0300

    Actualizcion del repo como as is. Lo emprolijo luego y hago otro commit.

---
+++ 06_getPost/controllers/usersController.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+// let users = require('../data/users'); //NO USAR ASI. HACERLO CON FS y PARSEARLO dentro del método o crear una función independeintes que traiga los datos parseados y ejecutarla dentro del método.
+//El archivo json debe arrancar vacío. Cuando se cargue el primer usuario se creará el array.
+
+
+const usersController = {
+    //Muestra un listado de usarios
+    index:function(req, res){
+        let users = fs.readFileSync(__dirname + '/../data/users.json', 'utf8');
+        users= JSON.parse(users);
+        res.render('users', {users: users});
+    },
+    //Mostar formulario de creación de recurso
+    create: function(req, res){
+        res.render('register');
+    },
+    //Guardar datos del usuario en json
+    store: function(req,res){
+        //Crear un usuario
+        let user = {
+            id:'',
+            email:req.body.email,
+            password: req.body.password, 
+        } //Más adelante vamoa a hashear la contraseña.
+
+        //Agregar al usuario en la base de datos.
+        let users = fs.readFileSync(__dirname + '/../data/users.json', 'utf8');
+        users = [...users, user]; //Tengo el array de usuarios en formato de objeto
+        users = JSON.stringify(users, null, ' ');
+
+        //Guardarlo
+        fs.writeFileSync(__dirname + '/../data/users.json', users);
+
+        //redirigir a otrea vista
+        res.redirect('/registro/users');
+    }
+}
+
+
+module.exports = usersController;
