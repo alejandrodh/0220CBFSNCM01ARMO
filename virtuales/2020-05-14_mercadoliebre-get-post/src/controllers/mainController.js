2c56bf16df0bc1a48cb4d61567a706ffb95de2e1
alejandrodh <alejandro@digitalhouse.com>
2020-05-19 05:22:14 -0300

    Get y post. Multer. Clase virutal de repaso login y registro.

---
+++ virtuales/2020-05-14_mercadoliebre-get-post/src/controllers/mainController.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+const controller = {
+	root: (req, res) => {
+		// Do the magic
+		let visited = products.filter(function(product){
+			return product.category == 'visited'
+		});
+
+		let inSale = products.filter(function (product) {
+			return product.category == 'in-sale'
+		});
+		
+		res.render('index', {visited, inSale, toThousand})
+	},
+	search: (req, res) => {
+		// Do the magic
+		let productoBuscado = req.query.keywords;
+
+		let productsArray = products.filter(e => e.name == productoBuscado);
+
+		res.render('results', {products: productsArray});
+	},
+};
+
+module.exports = controller;
